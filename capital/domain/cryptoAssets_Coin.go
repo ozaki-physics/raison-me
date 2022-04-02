aed5c688ec0b3c1207f3ca44bfc83a484a55ad04
ozaki-physics <setuna.f.masato@gmail.com>
2022-04-02 21:13:15 +0900

    feat: 暗号資産の価格管理 ドメイン層を作る #4

---
+++ capital/domain/cryptoAssets_Coin.go
@@ -0,0 +1,35 @@
+package domain
+
+import "errors"
+
+type Coin struct {
+	symbol string  // 銘柄
+	bid    float64 // 現在値(売値)
+}
+
+// constructCoin コンストラクタ
+func constructCoin(symbol string, bid float64) (*Coin, error) {
+	if symbol == "" {
+		return nil, errors.New("銘柄を登録してください")
+	}
+
+	c := &Coin{
+		symbol,
+		bid,
+	}
+	return c, nil
+}
+
+// func createCoin(symbol string) (*Coin, error) {
+// return constructCoin(symbol)
+// }
+
+// reconstructCoin DB などの値からインスタンスを再構成
+func ReconstructCoin(symbol string, bid float64) (*Coin, error) {
+	return constructCoin(symbol, bid)
+}
+
+// ゲッター
+func (c *Coin) Bid() float64 {
+	return c.bid
+}
---
+++ capital/domain/cryptoAssets_CoinRepository.go
@@ -0,0 +1,10 @@
+package domain
+
+// CoinRepository コインのインタフェース
+// この struct は ユースケース層 で使われて 実装は インフラ層
+type CoinRepository interface {
+	// Create(c *Coin) (*Coin, error)
+	FindBySymbol(symbol string) (*Coin, error)
+	// Save(c *Coin) (*Coin, error)
+	// Delete(c *Coin) error
+}
