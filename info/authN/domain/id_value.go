1b4f3d96af89c03897dbc0639ce086339d765de5
ozaki-physics <setuna.f.masato@gmail.com>
2023-05-21 17:45:51 +0900

    feat: ID生成方法の調整

--- info/authN/domain/id_value.go
+++ info/authN/domain/id_value.go
@@ -1,23 +1,25 @@
 package domain
 
 import (
+	"strings"
+
 	"github.com/rs/xid"
 )
 
 type id struct {
 	prefixID string
-	value    xid.ID
+	value    string
 }
 
-func NewID(prefixID string) (id, error) {
-	// バリデーションが先だが 値オブジェクトを返すために id が nil だとダメだから
-	guid := xid.New()
-	id := id{prefixID, guid}
-
+func constructorID(prefixID string, val string) (id, error) {
 	if prefixID == "" {
-		return id, NewDomainError("ID生成時のプレフィックスが存在しません")
+		return NilID(), NewDomainError("ID生成時のプレフィックスが存在しません")
+	}
+	if val == "" {
+		return NilID(), NewDomainError("ID生成時の値が存在しません")
 	}
 
+	id := id{prefixID, val}
 	if len(id.Val()) > 30 {
 		return id, NewDomainError("ID生成時のプレフィックスが長すぎます")
 	}
@@ -25,9 +27,30 @@ func NewID(prefixID string) (id, error) {
 	return id, nil
 }
 
+func NewID(prefixID string) (id, error) {
+	guid := xid.New()
+	return constructorID(prefixID, guid.String())
+}
+
+func ReNewID(data string) (id, error) {
+	sp := strings.Split(data, "-")
+	var val string
+	if len(sp) >= 3 {
+		val = strings.Join(sp[1:], "-")
+	} else {
+		val = sp[1]
+	}
+	return constructorID(sp[0], val)
+}
+
+func NilID() id {
+	nilID, _ := constructorID("nil", "nil")
+	return nilID
+}
+
 // 以下ゲッター
 func (id *id) Val() string {
-	return id.prefixID + "-" + id.value.String()
+	return id.prefixID + "-" + id.value
 }
 
 func (id *id) PrefixID() string {
--- info/authN/domain/id_value_test.go
+++ info/authN/domain/id_value_test.go
@@ -104,3 +104,169 @@ func TestNewID(t *testing.T) {
 		})
 	}
 }
+
+func TestNilID(t *testing.T) {
+	// テスト対象に渡す必要がある引数
+	type args struct {
+	}
+	type want struct {
+		first  string
+		second string
+	}
+	type err struct {
+		hasErr bool
+		msg    string
+	}
+	// テスト用の値たち
+	tests := []struct {
+		name string
+		args args
+		want want
+		err  err
+	}{
+		// テストケース
+		{
+			name: "nilなIDが生成できるか?",
+			args: args{},
+			want: want{
+				first:  "nil",
+				second: "nil",
+			},
+			err: err{
+				hasErr: false,
+				msg:    "",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := domain.NilID()
+
+			if got.Val() != tt.want.first+"-"+tt.want.second {
+				t.Errorf("実際の値は %v, 想定した値は %v", got.Val(), tt.want)
+			}
+		})
+	}
+}
+
+func TestReNewID(t *testing.T) {
+	// テスト対象に渡す必要がある引数
+	type args struct {
+		data string
+	}
+	type want struct {
+		first  string
+		second string
+	}
+	type err struct {
+		hasErr bool
+		msg    string
+	}
+	// テスト用の値たち
+	tests := []struct {
+		name string
+		args args
+		want want
+		err  err
+	}{
+		// テストケース
+		{
+			name: "文字列を渡して生成できるか?",
+			args: args{
+				data: "u-asdf",
+			},
+			want: want{
+				first:  "u",
+				second: "asdf",
+			},
+			err: err{
+				hasErr: false,
+				msg:    "",
+			},
+		},
+		{
+			name: "文字列にハイフンがあっても生成できるか?",
+			args: args{
+				data: "u-asdf-ghjk",
+			},
+			want: want{
+				first:  "u",
+				second: "asdf-ghjk",
+			},
+			err: err{
+				hasErr: false,
+				msg:    "",
+			},
+		},
+		{
+			name: "文字列の後半が無いとエラーになるか?",
+			args: args{
+				data: "u-",
+			},
+			want: want{
+				first:  "u",
+				second: "",
+			},
+			err: err{
+				hasErr: true,
+				msg:    "ID生成時の値が存在しません",
+			},
+		},
+		{
+			name: "文字列の前半が無いとエラーになるか?",
+			args: args{
+				data: "-asdf",
+			},
+			want: want{
+				first:  "",
+				second: "asdf",
+			},
+			err: err{
+				hasErr: true,
+				msg:    "ID生成時のプレフィックスが存在しません",
+			},
+		},
+	}
+
+	// テスト関数の実行前
+	// 処理
+	defer func() {
+		// テスト関数の実行後
+		// 処理
+	}()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// テストケースの実行前
+			// 処理
+			defer func() {
+				// テストケースの実行後
+				// 処理
+			}()
+
+			got, err := domain.ReNewID(tt.args.data)
+
+			if (err != nil) != tt.err.hasErr {
+				t.Errorf("実際の error = %v, hasErr %v", err, tt.err.hasErr)
+				return
+			}
+
+			if (err != nil) && tt.err.hasErr {
+				var de = domain.NewDomainError("")
+				if errors.As(err, &de) {
+					if err.Error() != tt.err.msg {
+						t.Errorf("実際のエラーは %v, 想定されるエラーは %v", err.Error(), tt.err.msg)
+					}
+				} else {
+					t.Errorf("実際の error = %v, hasErr %v", err, tt.err.hasErr)
+				}
+				return
+			}
+
+			if got.Val() != tt.want.first+"-"+tt.want.second {
+				t.Errorf("実際の値は %v, 想定した値は %v", got.Val(), tt.want)
+			}
+		})
+	}
+}
