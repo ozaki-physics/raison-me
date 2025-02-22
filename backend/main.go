320976f5d909485a1be902e135ec608cc5ee67af
ozaki-physics <setuna.f.masato@gmail.com>
2025-02-22 21:56:04 +0900

    chore: GitHub Actions の設定を追加

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World ! %s", time.Now())
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", greet)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Could not listen on port %s %v", port, err)
+	}
 }
