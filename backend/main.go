acfad49aac4d315ce81ac6c1834542d8799957a3
ozaki-physics <setuna.f.masato@gmail.com>
2025-03-05 00:02:47 +0900

    feat: backend 一時的に退避したコードを一部戻す

--- backend/main.go
+++ backend/main.go
@@ -2,25 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"time"
+
+	"github.com/ozaki-physics/raison-me/helloworld"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World ! %s", time.Now())
-}
-
 func main() {
-	http.HandleFunc("/", greet)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8081"
-	}
-
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("Could not listen on port %s %v", port, err)
-	}
+	fmt.Println("hello world!")
+	helloworld.Main()
 }
