1f136de655f206e871c18c08235c891366e4faa1
ozaki-physics <setuna.f.masato@gmail.com>
2026-01-17 03:32:30 +0000

    feat: Cloud Run の Secret Manager をマウントする仕様に伴って コードを変更

--- backend/share/config/global_config.go
+++ backend/share/config/global_config.go
@@ -26,12 +26,11 @@ func NewConfig() Config {
 		port = "8081"
 	}
 
-	sc := newSecretConfig(isCloud)
-	isLive := sc.readFile("IS_LIVE") == "true"
-	gcpProjectID := sc.readFile("GCP_PROJECT_ID")
+	isLive := readFile[string](isCloud, "IS_LIVE") == "true"
+	gcpProjectID := readFile[string](isCloud, "GCP_PROJECT_ID")
 
-	dataSourceName := sc.readFile("DATABASE_URL")
-	supabaseConfig := readJSONFile[supabaseConfig](sc, "DATABASE_SUPABASE_JSON")
+	dataSourceName := readFile[string](isCloud, "DATABASE_URL")
+	supabaseConfig := readFile[supabaseConfig](isCloud, "DATABASE_SUPABASE_JSON")
 
 	c := config{
 		isLive:         isLive,
@@ -80,40 +79,6 @@ func (c *config) GetPort() string {
 	return c.port
 }
 
-// secretConfig は secret file の 読み取り に関する 設定 を 管理
-type secretConfig struct {
-	basePath string
-}
-
-func newSecretConfig(isCloud bool) *secretConfig {
-	// secret file が 格納されてる ディレクトリ の パスを取得
-	basePath := "./share/secrets/"
-	if isCloud {
-		// /app は Cloud Run 用 の Dockerfile で 作成 した ディレクトリ
-		basePath = "/app/share/secrets/"
-	}
-
-	return &secretConfig{
-		basePath: basePath,
-	}
-}
-
-// 秘密情報 ファイル を 読み取る ヘルパー 関数
-func (sc *secretConfig) readFile(fileName string) string {
-	filePath := sc.basePath + fileName
-	b, err := os.ReadFile(filePath)
-	if err != nil {
-		// TODO: エラーハンドリング が 雑
-		log.Printf("Failed to read file: %v", err)
-		return ""
-	}
-
-	value := string(b)
-	// 改行コード を 削除
-	value = strings.TrimSpace(value)
-	return value
-}
-
 type supabaseConfig struct {
 	User            string `json:"user"`
 	Password        string `json:"password"`
@@ -124,8 +89,35 @@ type supabaseConfig struct {
 
 // 機密情報 JSON ファイル を 読み取る ヘルパー 関数
 // (ジェネリクス 使用のため レシーバー にできない)
-func readJSONFile[T any](sc *secretConfig, fileName string) T {
-	filePath := filepath.Join(sc.basePath, fileName)
+func readFile[T any](isCloud bool, fileName string) T {
+	// ファイル パス の 組み立て
+	var filePath string
+	if isCloud {
+		// なぜ fileName を 2回 繰り返すか
+		// Cloud Run では Secret Manager は 1つの シークレット に 1つの ボリューム を 対応 させる 必要がある
+		// また ボリューム を 1個のディレクトリに まとめられない
+		// よって シークレット名 ごとに ディレクトリ が 作成 されるため
+
+		// /app は Cloud Run 用 の Dockerfile で 作成 した ディレクトリ
+		filePath = filepath.Join("/app/share/secrets/", fileName, fileName)
+	} else {
+		// secret file が 格納されてる ディレクトリ の パスを取得
+		filePath = filepath.Join("./share/secrets/", fileName)
+	}
+
+	var zero T
+	if _, ok := any(zero).(string); ok {
+		b, err := os.ReadFile(filePath)
+		if err != nil {
+			// TODO: エラーハンドリング が 雑
+			log.Printf("Failed to read file: %v", err)
+			return zero
+		}
+
+		s := strings.TrimSpace(string(b))
+		return any(s).(T)
+	}
+
 	b, err := os.ReadFile(filePath)
 	if err != nil {
 		// TODO: エラーハンドリング が 雑
